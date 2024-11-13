290e428f5bf50c0014f147feb549368fb0d9212f
Leauxgan1 <logan.langdon@yahoo.com>
2024-11-12 17:55:22 -0800

    Refactored website layout for a neobrutalist style, organized constants into respective ts file

---
+++ src/constants.ts
@@ -0,0 +1,25 @@
+
+
+//export const SiteColors = {
+//  primary: "#19a83f",
+//  secondary: "#044d1c",
+//  backgroundLight: "#ababab",
+//  backgroundDark: "#333333",
+//};
+
+export const SiteInfo = {
+  title: "Logan's Portfolio",
+  description: "The professional portfolio for Logan Langdon, a software developer",
+  tags: ["neobrutalist","portfolio","software","static"],
+  sections: ["home","about","projects","experience","contact"],
+};
+
+export const ExternLinks = {
+  blog: "www.google.com",
+  github: "https://github.com/leauxgan1",
+  mastodon: "www.google.com",
+  linkedin: "https://www.linkedin.com/in/logan-langdon-55933018a/",
+}
+
+
+
