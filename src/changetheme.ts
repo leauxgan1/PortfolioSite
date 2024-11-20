3ac1d92e96a13ab4ca05ec85cc3e98d047f60edd
Leauxgan1 <logan.langdon@yahoo.com>
2024-11-19 18:22:58 -0800

    Added dark-light feature and styles

--- src/changetheme.ts
+++ src/changetheme.ts
@@ -1,28 +1,30 @@
-function swapTheme() {
-  var element = document.body;
-  element.classList.toggle("dark-theme");
+
+function enableThemeSwap() {
+  //document.body.classList.toggle("swapped-mode");
+  //console.log(document.body.classList);
   const buttons = document.querySelectorAll(".theme-toggle");
   if (!buttons) {
     return;
   }
-  const currentTheme = localStorage.getItem("theme");
-  if (currentTheme == "dark") {
-    document.body.classList.add("dark-theme");
-  }
-
+  //const currentTheme = localStorage.getItem("theme");
+  //console.log(currentTheme);
+  //if (currentTheme == "dark") {
+  //  document.body.classList.add("dark-theme");
+  //}
+  //
   buttons.forEach((button) => {
     button.addEventListener('click', () => {
       console.log("Swapping themes...");
-      document.body.classList.toggle("dark-theme");
-      let theme = "light";
-      if (document.body.classList.contains("dark-theme")) {
-        theme = "dark";
-      }
-      localStorage.setItem("theme", theme);
+      document.documentElement.classList.toggle("swapped-mode");
+      //let theme = "light";
+      //if (document.body.classList.contains("dark-theme")) {
+      //  theme = "dark";
+      //}
+      //localStorage.setItem("theme", theme);
     });
-
+  
   });
 }
 
-swapTheme();
+enableThemeSwap();
 
