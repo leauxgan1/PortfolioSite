0f1bbdd0813936a9c973ea8a95c1069d504fa33f
Leauxgan1 <logan.langdon@yahoo.com>
2024-11-28 14:15:28 -0800

    Added github actions to build and deploy the site

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "static",
+  site: "https://leauxgan1.github.io/",
   integrations: [icon()],
 });
